1139277e9f144f878a1e77919f4023b9d464207d
MarioWizRaccoon <mario.lafleur.1@ulaval.ca>
2019-03-26 20:18:48 -0400

    add hot loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+  import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
 class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, Johnny !</h1>
+        <h1> Allo Zara-Romane Lafleur!</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
